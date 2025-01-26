3181f2244c30b5031534dab9590b8ad411f4f0a6
Jason Thompson <jasonxandrewth@gmail.com>
2025-01-26 02:37:20 +0100

    Init 2

--- src/components/Experience/components/ImagePlane.js
+++ src/components/Experience/components/ImagePlane.js
@@ -4,8 +4,8 @@ import { useLoader, useFrame, useThree, extend } from "@react-three/fiber";
 import { useTexture, shaderMaterial } from "@react-three/drei";
 
 //Shaders
-import vertex from "../Materials/shaders/background.vert";
-import fragment from "../Materials/shaders/background.frag";
+// import vertex from "../Materials/shaders/background.vert";
+// import fragment from "../Materials/shaders/background.frag";
 
 const DiffuseMaterial = shaderMaterial(
   {
@@ -15,8 +15,80 @@ const DiffuseMaterial = shaderMaterial(
     uTextureDiffuseNext: null,
     uColor: new THREE.Color(0x000000),
   },
-  vertex,
-  fragment
+  `
+  attribute vec2 uvref;
+attribute float aSize;
+
+uniform float uTime;
+uniform float uSize;
+uniform vec2 uRes;
+uniform sampler2D uTextureDepth;
+
+varying vec2 vUv;
+
+void main() {
+    vUv = uv;
+
+    vec3 newpos = position;
+
+      // Final position
+    vec4 modelPosition = modelMatrix * vec4(newpos, 1.0);
+    vec4 viewPosition = viewMatrix * modelPosition;
+    vec4 projectedPosition = projectionMatrix * viewPosition;
+
+    gl_Position = projectedPosition;
+
+}
+
+  `,
+  `
+  varying vec2 vUv;
+uniform float uTime;
+uniform vec3 uColor;
+uniform sampler2D uTextureDiffuse;
+uniform sampler2D uTextureDiffuseNext;
+
+float luminance(vec4 color) {
+    return 0.2126 * color.r + 0.7152 * color.g + 0.0722 * color.b;
+}
+
+void main() {
+
+    vec3 color = vec3(0.0, 0.0, 0.0);
+    color = uColor;
+    color.rg = vUv;
+    vec2 smoothUV = smoothstep(0.0, 0.1, vUv);
+    smoothUV *= vec2(smoothstep(0.0, 0.1, 1.0 - vUv));
+
+    vec4 textureCurrent = texture2D(uTextureDiffuse, vUv);
+    vec4 textureNext = texture2D(uTextureDiffuseNext, vUv);
+
+    vec2 scaledUv = vUv * vec2(1.0);
+
+    float luminanceDifference = abs(luminance(textureNext) - luminance(textureCurrent));
+
+    vec2 colorDifferenceRG = textureNext.rg - textureCurrent.rg;
+    vec2 direction = normalize(colorDifferenceRG + sin(uTime) * 0.5);
+
+    //color1 in example touchdesigner
+    scaledUv.rg += direction * (luminanceDifference * (sin(uTime) * 0.5 + 1.0) * smoothUV);
+
+    // color.rg = scaledUv;
+    scaledUv *= 0.05;
+
+    // color.rgb = vec3(0.0);
+    vec4 textureCurrentDisplaced = texture2D(uTextureDiffuse, vUv + scaledUv);
+    vec4 textureNextDisplaced = texture2D(uTextureDiffuseNext, vUv + scaledUv);
+    vec3 texNow = mix(textureCurrentDisplaced.rgb, textureNextDisplaced.rgb, (sin(uTime * 0.5) + 1.0) * 0.5);
+    color.rgb = texNow.rgb;
+    // color.rgb = vec3((sin(uTime) + 1.0) * 0.5);
+
+    gl_FragColor = vec4(color, 0.3);
+
+    #include <tonemapping_fragment>
+    #include <colorspace_fragment>
+}
+  `
 );
 extend({ DiffuseMaterial });
 
